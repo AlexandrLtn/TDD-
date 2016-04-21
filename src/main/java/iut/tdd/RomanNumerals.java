4f49020af8f97075d939c2816bf0219849875ddc
Alexandre Lantoine <lantoine.alexandre@outlook.fr>
2016-04-21 15:57:50 +0200

    2

--- src/main/java/iut/tdd/RomanNumerals.java
+++ src/main/java/iut/tdd/RomanNumerals.java
@@ -3,12 +3,9 @@ package iut.tdd;
 public class RomanNumerals {
 	public String convertToRoman(String arabe) {
 		String res="";
-		for(int i=0;i<=3;i++){
-			if(arabe=="3"){
-				res="III";
-			}else if(arabe=="1"){
-				res="I";
-			}
+		int nb=Integer.parseInt(arabe);
+		for(int i=0;i<nb;i++){
+			res=res+"I";
 		}
 		return res;
 	}
